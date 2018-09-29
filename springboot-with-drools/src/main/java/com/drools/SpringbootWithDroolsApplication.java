66b28e9e1674335ccab58d1e22e3e003d9c638ca
BlueSevenYa <1395596836@qq.com>
2018-09-29 22:46:51 +0800

    初次提交drools，目前有错，报找不到drl文件

---
+++ springboot-with-drools/src/main/java/com/drools/SpringbootWithDroolsApplication.java
@@ -0,0 +1,12 @@
+package com.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithDroolsApplication.class, args);
+	}
+}
