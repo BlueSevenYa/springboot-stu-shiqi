ef3d96c5496ea2f12e98968fe999ae57dca0f3e7
BlueSevenYa <1395596836@qq.com>
2018-10-30 22:18:05 +0800

    export excel 测试例子导入

---
+++ springboot-export-import-excel/src/main/java/com/excel/entity/User.java
@@ -0,0 +1,39 @@
+package com.excel.entity;
+
+/**
+ * Created by
+ *
+ * @author=蓝十七
+ * @on 2018-10-30-21:46
+ */
+
+public class User {
+
+    private String name;
+
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
--- springboot-export-import-excel/src/test/java/com/excel/SpringbootExportImportExcelApplicationTests.java
+++ springboot-export-import-excel/src/test/java/com/excel/SpringbootExportImportExcelApplicationTests.java
@@ -1,14 +1,17 @@
 package com.excel;
 
+import com.excel.entity.User;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -30,6 +33,39 @@ public class SpringbootExportImportExcelApplicationTests {
 		FileOutputStream output=new FileOutputStream("d:\\workbook.xls");
 		workbook.write(output);
 		output.flush();
+	}
+
+	@Test
+	public void test2() throws IOException {
+		System.out.println("ss");
+		FileInputStream in=new FileInputStream("C:\\Users\\dell\\Desktop\\user1.xls");
+		HSSFWorkbook workbook=new HSSFWorkbook(in);
+		HSSFSheet sheet=workbook.getSheetAt(0);
+		for(int i=1;i<=sheet.getLastRowNum();i++){
+			Row row=sheet.getRow(i);
+			User user=new User();
+			user.setName((row.getCell(0)).toString());
+			user.setAddress((row.getCell(1)).toString());
+			System.out.println(user);
+		}
+	}
+
+	@Test
+	public void test3() throws IOException {
+		FileInputStream in=new FileInputStream("C:\\Users\\dell\\Desktop\\user1.xls");
+		HSSFWorkbook workbook=new HSSFWorkbook(in);
+		HSSFSheet sheet=workbook.getSheetAt(0);
+		for(Row r:sheet){
+			System.out.println(r.getRowNum());
+			if(r.getRowNum()<1){
+				continue;
+			}
+			User user=new User();
+			user.setName(r.getCell(0).getStringCellValue());
+			user.setAddress(r.getCell(1).getStringCellValue());
+			System.out.println(user);
+		}
+		in.close();
 	}
 
 }
